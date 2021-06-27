1ff499a8b5c066e06ae5d0841f8cb70082c87462
Gabriel Zanluca <gabriel.zanluca@gmail.com>
2021-06-26 21:26:15 -0300

    Build Home Screen

---
+++ src/utils/Const.js
@@ -0,0 +1,16 @@
+const IMAGE_VARIANT = {
+  portrait: {
+    portrait_small: 'portrait_small'
+  },
+  standard: {
+    standard_small: 'standard_small',
+    standard_medium: 'standard_medium',
+    standard_large: 'standard_large',
+    standard_xlarge: 'standard_xlarge',
+    standard_fantastic: 'standard_fantastic'
+  }
+}
+
+const test = ''
+
+export { IMAGE_VARIANT, test }
